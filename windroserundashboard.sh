d2d1ae826be42176c050a9291a4d239abb113289
Greelan <53196309+Greelan@users.noreply.github.com>
2026-04-23 14:00:15 +1000

    Update windroserundashboard.sh

--- windroserundashboard.sh
+++ windroserundashboard.sh
@@ -22,7 +22,7 @@ if ! $serverStarted; then
 fi
 
 # Start the Windrose+ dashboard
-cd "./windrose/4129620"
+cd ./windrose/4129620
 powershell/pwsh -NoProfile -File "$scriptDir/windrose/4129620/windrose_plus/server/windrose_plus_server.ps1" -Port $1 -GameDir "$scriptDir/windrose/4129620" &
 dashboardPid=$!
 
